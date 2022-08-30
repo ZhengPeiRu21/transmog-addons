4130a10142c8760f08c0a1b662ce0f9654d9e644
郑佩茹 <zhengpeiru21@protonmail.com>
2022-08-30 11:34:44 -0600

    Fix items with no appearance showing tooltip (bags, etc.). Add sync command support.

--- transmogTip/core.lua
+++ transmogTip/core.lua
@@ -15,7 +15,11 @@ local function attachItemTooltip(self)
 	local link = select(2,self:GetItem())
 	if not link then return end
 	local id = select(3,strfind(link, "^|%x+|Hitem:(%-?%d+):(%d+):(%d+):(%d+):(%d+):(%d+):(%-?%d+):(%-?%d+)"))
-	if IsEquippableItem(id) and not tContains(TransmogTipList, tonumber(id)) then 
+	itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(id)
+	if itemEquipLoc == "INVTYPE_AMMO" or itemEquipLoc == "INVTYPE_NECK" or itemEquipLoc == "INVTYPE_FINGER" or itemEquipLoc == "INVTYPE_TRINKET" or itemEquipLoc == "INVTYPE_BAG" or itemEquipLoc == "INVTYPE_QUIVER" or tContains(TransmogTipList, tonumber(id)) then 
+    return
+  end
+	if IsEquippableItem(id) then 
     addLine(self,id,true)
   end
 end
@@ -44,7 +48,17 @@ function onEvent(self, event, arg1, ...)
     end
   end
 end
-  
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", function(self, event, msg, ...)
+  if msg:find("TRANSMOG_SYNC:") then
+    itemIDStr = string.gsub(msg, "TRANSMOG_SYNC:", "")
+    itemID = tonumber(itemIDStr)
+    if not tContains(TransmogTipList, itemID) then
+      table.insert(TransmogTipList, itemID)
+    end
+    return true
+  end
+end)
 GameTooltip:HookScript("OnTooltipSetItem", attachItemTooltip)
 ItemRefTooltip:HookScript("OnTooltipSetItem", attachItemTooltip)
 ItemRefShoppingTooltip1:HookScript("OnTooltipSetItem", attachItemTooltip)
